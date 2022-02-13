ce68bc206c97440f139a090b6f12884d170960a5
linzhanguca <lzhang12@uca.edu>
2022-02-13 15:53:17 -0600

    fix package name in launch

--- launch/view_bot.launch.py
+++ launch/view_bot.launch.py
@@ -8,54 +8,72 @@ from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
+
 def generate_launch_description():
-    urdf_tutorial_path = get_package_share_path('urdf_demo')
-    default_model_path = urdf_tutorial_path / 'urdf/bot.urdf.xacro'
-    default_rviz_config_path = urdf_tutorial_path / 'rviz/bot.rviz'
+    urdf_package_path = get_package_share_path("urdf_demo")
+    default_model_path = urdf_package_path / "urdf/bot.urdf.xacro"
+    default_rviz_config_path = urdf_package_path / "rviz/bot.rviz"
 
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
-                                    description='Flag to enable joint_state_publisher_gui')
-    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
-                                      description='Absolute path to robot urdf file')
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
-                                     description='Absolute path to rviz config file')
+    gui_arg = DeclareLaunchArgument(
+        name="gui",
+        default_value="true",
+        choices=["true", "false"],
+        description="Flag to enable joint_state_publisher_gui",
+    )
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=str(default_model_path),
+        description="Absolute path to robot urdf file",
+    )
+    rviz_arg = DeclareLaunchArgument(
+        name="rvizconfig",
+        default_value=str(default_rviz_config_path),
+        description="Absolute path to rviz config file",
+    )
 
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
-                                       value_type=str)
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model")]), value_type=str
+    )
 
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {
+                "robot_description": robot_description,
+            }
+        ],
     )
 
     # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
     joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        condition=UnlessCondition(LaunchConfiguration('gui'))
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        condition=UnlessCondition(LaunchConfiguration("gui")),
     )
 
     joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        condition=IfCondition(LaunchConfiguration("gui")),
     )
 
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
     )
 
-    return LaunchDescription([
-        gui_arg,
-        model_arg,
-        rviz_arg,
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        rviz_node
-    ])
+    return LaunchDescription(
+        [
+            gui_arg,
+            model_arg,
+            rviz_arg,
+            joint_state_publisher_node,
+            joint_state_publisher_gui_node,
+            robot_state_publisher_node,
+            rviz_node,
+        ]
+    )
